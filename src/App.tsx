3b96be8f1b8a2c7716f568226c81766dd2e4f383
metjyc <metjyc@naver.com>
2025-12-31 09:44:11 +0900

    feat: 라우터 연결 + 홈 페이지 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
-function App() {
-  return <div></div>;
-}
+import { Routes, Route, Navigate } from 'react-router-dom';
+import AppShell from './app/AppShell';
 
-export default App;
+import HomePage from './pages/HomePage';
+import LedgerPage from './pages/LedgerPage';
+import FreezePage from './pages/FreezePage';
+import ReportPage from './pages/ReportPage';
+import MyPage from './pages/MyPage';
+
+export default function App() {
+  return (
+    <Routes>
+      {/* AppShell을 사용하는 모바일 앱 영역 */}
+      <Route element={<AppShell />}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/ledger" element={<LedgerPage />} />
+        <Route path="/freeze" element={<FreezePage />} />
+        <Route path="/report" element={<ReportPage />} />
+        <Route path="/my" element={<MyPage />} />
+      </Route>
+
+      {/* 그 외 예외 처리 */}
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  );
+}
