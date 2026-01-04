2de48f9e5c40682e8c2e96beb258d35550d0078a
jivvonC <jiwonchon07@gmail.com>
2026-01-04 16:46:51 +0900

    style: freeze ui layout wip

--- src/pages/FreezePage.tsx
+++ src/pages/FreezePage.tsx
@@ -1,7 +1,209 @@
 export default function FreezePage() {
   return (
     <>
-      <h1>FreezePage Component</h1>
+      <div data-layer="냉동 화면" className="w-96 h-[812px] relative bg-white_800 overflow-hidden">
+        <div
+          data-layer="menu"
+          data-property-1="freeze_on"
+          className="Menu w-96 h-20 left-[-1px] top-[54px] absolute bg-white_800 inline-flex justify-between items-end"
+        >
+          <div
+            data-layer="Frame 48096422"
+            className="Frame48096422 flex-1 p-2.5 shadow-[0px_0px_10px_0px_rgba(94,151,215,1.00)] border-b border-main_skyblue flex justify-center items-center gap-2.5"
+          >
+            <div
+              data-layer="냉동하기"
+              className="text-center justify-start text-main_skyblue text-lg font-medium font-['Pretendard'] leading-7 tracking-tight"
+            >
+              냉동하기
+            </div>
+          </div>
+          <div
+            data-layer="Frame 48096423"
+            className="Frame48096423 flex-1 p-2.5 border-b border-gray_400 flex justify-center items-center gap-2.5"
+          >
+            <div
+              data-layer="냉동 기록"
+              className="text-center justify-start text-gray_400 text-lg font-medium font-['Pretendard'] leading-7 tracking-tight"
+            >
+              냉동 기록
+            </div>
+          </div>
+        </div>
+        <div
+          data-layer="Frame 48096424"
+          className="Frame48096424 w-80 h-[546px] left-[24px] top-[154px] absolute bg-white_800 rounded-[20px] shadow-[0px_0px_8px_0px_rgba(0,0,0,0.20)] overflow-hidden"
+        >
+          <div
+            data-layer="Frame 48096429"
+            className="Frame48096429 w-80 px-7 py-2.5 left-0 top-[88px] absolute inline-flex flex-col justify-start items-start gap-2.5"
+          >
+            <div
+              data-layer="Frame 48096427"
+              className="Frame48096427 self-stretch inline-flex justify-start items-center gap-4"
+            >
+              <img
+                data-layer="image 150"
+                className="Image150 w-14 h-14 rounded-lg border border-gray_200"
+                src="https://placehold.co/54x54"
+              />
+              <img
+                data-layer="image 151"
+                className="Image151 w-14 h-14 rounded-lg"
+                src="https://placehold.co/54x54"
+              />
+              <img
+                data-layer="image 152"
+                className="Image152 w-14 h-14 rounded-lg"
+                src="https://placehold.co/54x54"
+              />
+              <img
+                data-layer="image 153"
+                className="Image153 w-14 h-14 rounded-lg border border-gray_200"
+                src="https://placehold.co/54x54"
+              />
+            </div>
+          </div>
+          <div
+            data-layer="Frame 48096433"
+            className="Frame48096433 w-80 px-7 py-2.5 left-0 top-[162px] absolute inline-flex flex-col justify-start items-start gap-2.5"
+          >
+            <div
+              data-layer="Frame 48096427"
+              className="Frame48096427 self-stretch inline-flex justify-start items-center gap-4"
+            >
+              <div
+                data-layer="application"
+                data-property-1="plus"
+                className="Application w-14 h-14 relative bg-white_400 rounded-lg overflow-hidden"
+              >
+                <div
+                  data-layer="ic"
+                  data-property-1="ic_plus"
+                  className="Ic w-6 h-6 left-[15px] top-[15px] absolute overflow-hidden"
+                >
+                  <div
+                    data-layer="Vector"
+                    className="Vector w-4 h-4 left-[4px] top-[4px] absolute outline outline-2 outline-offset-[-1px] outline-gray_400"
+                  ></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            data-layer="초기화"
+            className="left-[270px] top-[41px] absolute text-center justify-start text-gray_200 text-sm font-normal font-['Pretendard'] underline leading-5 tracking-tight"
+          >
+            초기화
+          </div>
+          <div
+            data-layer="ic"
+            data-property-1="ic_left"
+            className="Ic w-6 h-6 left-[4px] top-[150px] absolute overflow-hidden"
+          >
+            <div
+              data-layer="Vector"
+              className="Vector w-1.5 h-3 left-[7.94px] top-[6.22px] absolute bg-gray_200"
+            ></div>
+          </div>
+          <div
+            data-layer="ic"
+            data-property-1="ic_right"
+            className="Ic w-6 h-6 left-[300px] top-[150px] absolute overflow-hidden"
+          >
+            <div
+              data-layer="Vector"
+              className="Vector w-1.5 h-3 left-[8.47px] top-[6.22px] absolute bg-gray_200"
+            ></div>
+          </div>
+          <div
+            data-layer="Frame 48096432"
+            className="Frame48096432 w-72 h-56 left-[20px] top-[291px] absolute bg-sub_skyblue rounded-bl-[20px] rounded-br-[20px] overflow-hidden"
+          >
+            <div
+              data-layer="Frame 48096433"
+              className="Frame48096433 w-72 px-5 left-0 top-[39.50px] absolute inline-flex justify-start items-center gap-2.5"
+            >
+              <div
+                data-layer="금액 원의 품목 을 결제할지 고민이에요"
+                className="flex-1 justify-start"
+              >
+                <span className="text-white_800_40/40 text-xl font-medium font-['Pretendard'] underline leading-8 tracking-tight">
+                  금액
+                </span>
+                <span className="text-gray_800 text-xl font-medium font-['Pretendard'] leading-8 tracking-tight">
+                  {' '}
+                  원의
+                  <br />
+                </span>
+                <span className="text-white_800_40/40 text-xl font-medium font-['Pretendard'] underline leading-8 tracking-tight">
+                  품목
+                </span>
+                <span className="text-gray_800 text-xl font-medium font-['Pretendard'] leading-8 tracking-tight">
+                  {' '}
+                  을<br />
+                  결제할지 고민이에요
+                </span>
+              </div>
+            </div>
+          </div>
+          <div
+            data-layer="toggel"
+            data-property-1="passivity"
+            className="Toggel w-72 h-12 px-4 py-2 left-[20px] top-[266px] absolute bg-sub_skyblue rounded-[100px] inline-flex justify-center items-center overflow-hidden"
+          >
+            <div
+              data-layer="Button 1"
+              className="Button1 flex-1 self-stretch relative flex justify-start items-center"
+            >
+              <div
+                data-layer="Button"
+                className="Button w-32 h-9 px-2.5 py-[3px] left-[-2px] top-0 absolute bg-white_800 rounded-[20px] shadow-[0px_2px_10px_0px_rgba(0,0,0,0.06)]"
+              ></div>
+              <div
+                data-layer="Label"
+                className="Label flex-1 text-center justify-start text-gray_800 text-sm font-semibold font-['Pretendard'] leading-5 tracking-tight line-clamp-1"
+              >
+                수동 입력
+              </div>
+            </div>
+            <div
+              data-layer="Button 2"
+              className="Button2 flex-1 self-stretch flex justify-start items-center"
+            >
+              <div
+                data-layer="Label"
+                className="Label flex-1 text-center justify-start text-white_800 text-sm font-semibold font-['Pretendard'] leading-5 tracking-tight line-clamp-1"
+              >
+                링크 등록
+              </div>
+            </div>
+          </div>
+          <div
+            data-layer="button"
+            data-property-1="freeze_basic"
+            className="Button w-64 h-10 px-4 py-2 left-[36px] top-[463px] absolute bg-white_400 rounded-xl shadow-[0px_0px_8px_0px_rgba(0,0,0,0.20)] inline-flex justify-center items-center gap-2.5"
+          >
+            <div
+              data-layer="냉동하기"
+              className="flex-1 self-stretch text-center justify-center text-gray_200 text-base font-bold font-['Inter'] leading-6 tracking-tight"
+            >
+              냉동하기
+            </div>
+          </div>
+        </div>
+        <div
+          data-layer="Frame 32"
+          className="Frame32 w-96 px-6 left-0 top-[183px] absolute inline-flex justify-center items-center gap-2.5"
+        >
+          <div
+            data-layer="새로운 냉동하기"
+            className="flex-1 text-center justify-start text-gray_800 text-2xl font-bold font-['Pretendard'] leading-9"
+          >
+            새로운 냉동하기
+          </div>
+        </div>
+      </div>
     </>
   );
 }
