8151412c615c12e0be259a16f81d0a0526615bc2
metjyc <metjyc@naver.com>
2026-01-07 11:06:09 +0900

    feat: 이미지 컴포넌트 처리

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,4 +1,10 @@
-export {default as back} from './back.svg';
-export {default as close} from './close.svg';
-export {default as errorIcon} from './error.svg';
-export {default as verified} from './verified.svg';
+// image icons
+export { default as back } from './back.svg';
+export { default as close } from './close.svg';
+export { default as errorIcon } from './error.svg';
+export { default as verified } from './verified.svg';
+
+// react icons
+
+export { default as chevronLeftIcon } from '@/assets/icons/calendar/ic-chevron-left.svg';
+export { default as chevronRightIcon } from '@/assets/icons/calendar/ic-chevron-right.svg';
